b194adc5f8d3e9a6654ed980c4e8afd36a71689b
DouglasDL28 <dele18037@uvg.edu.gt>
2018-09-06 11:39:09 -0600

    Se agregaron las clases Board y Player.

---
+++ src/Board.java
@@ -0,0 +1,85 @@
+package Clases;
+
+
+import Clases.Properties.PrivateProperty;
+import Clases.Properties.Property;
+
+import java.util.ArrayList;
+
+public class Board {
+    private ArrayList<Player> players;
+    private ArrayList<Property> cells;
+    private Integer bank;
+    private Boolean PlayerOneTurn;
+    private Integer Height;
+    private Integer Width;
+    private ArrayList<PrivateProperty> freeCells;
+
+
+    public Board(Integer bank, Integer height, Integer width) {
+        this.players = new ArrayList<Player>();
+        this.cells = new ArrayList<Property>();
+        this.bank = bank;
+        PlayerOneTurn = true;
+        Height = height;
+        Width = width;
+        this.freeCells = new ArrayList<PrivateProperty>();
+    }
+
+
+    public void changeTurn () {
+        if (PlayerOneTurn) {
+            this.PlayerOneTurn = false;
+        } else {this.PlayerOneTurn = true;}
+    }
+
+    public Integer checkDoubles (Player player) {
+        
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public ArrayList<Property> getCells() {
+        return cells;
+    }
+
+    public void setCells(ArrayList<Property> cells) {
+        this.cells = cells;
+    }
+
+    public Integer getBank() {
+        return bank;
+    }
+
+    public void setBank(Integer bank) {
+        this.bank = bank;
+    }
+
+    public Boolean getPlayerOneTurn() {
+        return PlayerOneTurn;
+    }
+
+    public void setPlayerOneTurn(Boolean playerOneTurn) {
+        PlayerOneTurn = playerOneTurn;
+    }
+
+    public Integer getHeight() {
+        return Height;
+    }
+
+
+
+    public Integer getWidth() {
+        return Width;
+    }
+
+    public ArrayList<PrivateProperty> getFreeCells() {
+        return freeCells;
+    }
+
+    public void setFreeCells(ArrayList<PrivateProperty> freeCells) {
+        this.freeCells = freeCells;
+    }
+}
